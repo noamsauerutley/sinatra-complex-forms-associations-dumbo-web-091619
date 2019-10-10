a04c11da3a261987940fb52671ca07f42e7cda81
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-10-10 11:39:25 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class PetsController < ApplicationController
 
   get '/pets' do
@@ -6,12 +7,23 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
+    @pet = Pet.create(params[:pet])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+   @pet.save
+    redirect to "/pets/#{@pet.id}"
+  end
 
-    redirect to "pets/#{@pet.id}"
+  get '/pets/:id/edit' do
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+    erb :'/pets/edit'
   end
 
   get '/pets/:id' do 
@@ -20,7 +32,12 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
-
-    redirect to "pets/#{@pet.id}"
+      @pet = Pet.find(params[:id])
+      @pet.update(params["pet"])
+      if !params["owner"]["name"].empty?
+        @pet.owner = Owner.create(name: params["owner"]["name"])
+      end
+    @pet.save
+    redirect to "/pets/#{@pet.id}"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 # Add seed data here. Seed your database with `rake db:seed`
 sophie = Owner.create(name: "Sophie")
+harold = Owner.create(name: "Harold")
+elaine = Owner.create(name: "Elaine")
 Pet.create(name: "Maddy", owner: sophie)
 Pet.create(name: "Nona", owner: sophie)
+Pet.create(name: "Bippy", owner: elaine)
+Pet.create(name: "frank", owner: harold)
